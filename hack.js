b1d19608cf87a2277d216a4ad893344ec82143a9
kumaresh cj <cjkumaresh@gmail.com>
2016-06-17 23:59:08 +0530

    added changes to download videos and high resolution images

--- hack.js
+++ hack.js
@@ -1,19 +1,40 @@
-var instagramImageClassName = '._icyx7';
-    imagesSelector = document.querySelectorAll(instagramImageClassName);
+var instagramImageClassName = '._icyx7',
+	instagramVideoClassName = '._c8hkj';
 
-
-// Iterate the images and call download function
-Array.prototype.forEach.call(imagesSelector, function(el, i){
-    downloadImages(el.getAttribute('src'));
-});
-
-//download function to download image from the url
-function downloadImages(url) {
-	var a = document.createElement("a");
+//download function to download image/video from the url
+var download = (url) => {
+    var a = document.createElement("a");
 	document.body.appendChild(a);
 	a.style = "display: none";
-	a.href = url.replace('s640x640/sh0.08/', '').replace('c202.0.676.676/', ''); // to convert into full resolution images
+	a.href = url // to convert into full resolution images
 	a.download = url.split("?")[0].split("/")[url.split("?")[0].split("/").length - 1];
 	a.click();
 	window.URL.revokeObjectURL(url);
+
+}
+
+var downloadImages = () => {
+	var imagesSelector = document.querySelectorAll(instagramImageClassName);
+	// Iterate the images and call download function
+	Array.prototype.forEach.call(imagesSelector, (el, i) => {
+		var url = el.getAttribute('src');
+		url = url.split('/').filter((val, index) => { 
+			if(val.indexOf('c') === 0 || val === 'sh0.08' || val === 's640x640') 
+				return false;
+			else
+				return true;
+		}).join('/');
+		download(url);
+	});
+
+}
+
+var downloadVideo = () => {
+	var videoSelector = document.querySelectorAll(instagramVideoClassName);
+	// Iterate the images and call download function
+	Array.prototype.forEach.call(videoSelector, (el, i) => {
+		var url = el.getAttribute('src');
+		download(url);
+	});
+	
 }
