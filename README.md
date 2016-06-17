3bff4d27b3d54c7b871921c9f3af6b754c43a293
kumaresh cj <cjkumaresh@gmail.com>
2016-06-18 00:19:08 +0530

    updated readme and beautified the code

--- README.md
+++ README.md
@@ -1,13 +1,19 @@
 # download-instagram-images
-download images from instagram
+download high quality images and videos from instagram :)
 
 ## How To Use
 - Open developer tools
 - Go to instagram profile that you want to dowload images
-- Copy the js code from `hack.js` and then execute to download images
+- Copy the js code from `hack.js` and then execute
+- execute `downloadImages() // to download images`
+- execute `downloadVideos() // to download videos`
 - Make sure to enable `allow mutiple files download`
 
 Note: Tested only in chrome browser
+
+## Tip
+To download all images for a particular profile.
+- Go to the instagram profile and scroll till to the last image and then execute `downloadImages()`
 
 ## Important
 Instagram disables the user from downloading images. So use the script at your own risk
--- hack.js
+++ hack.js
@@ -1,40 +1,40 @@
 var instagramImageClassName = '._icyx7',
-	instagramVideoClassName = '._c8hkj';
+    instagramVideoClassName = '._c8hkj';
 
 //download function to download image/video from the url
 var download = (url) => {
     var a = document.createElement("a");
-	document.body.appendChild(a);
-	a.style = "display: none";
-	a.href = url // to convert into full resolution images
-	a.download = url.split("?")[0].split("/")[url.split("?")[0].split("/").length - 1];
-	a.click();
-	window.URL.revokeObjectURL(url);
+    document.body.appendChild(a);
+    a.style = "display: none";
+    a.href = url // to convert into full resolution images
+    a.download = url.split("?")[0].split("/")[url.split("?")[0].split("/").length - 1];
+    a.click();
+    window.URL.revokeObjectURL(url);
 
 }
 
 var downloadImages = () => {
-	var imagesSelector = document.querySelectorAll(instagramImageClassName);
-	// Iterate the images and call download function
-	Array.prototype.forEach.call(imagesSelector, (el, i) => {
-		var url = el.getAttribute('src');
-		url = url.split('/').filter((val, index) => { 
-			if(val.indexOf('c') === 0 || val === 'sh0.08' || val === 's640x640') 
-				return false;
-			else
-				return true;
-		}).join('/');
-		download(url);
-	});
+    var imagesSelector = document.querySelectorAll(instagramImageClassName);
+    // Iterate the images and call download function
+    Array.prototype.forEach.call(imagesSelector, (el, i) => {
+        var url = el.getAttribute('src');
+        // Convert the image (640x640) url into full resolution images url
+        url = url.split('/').filter((val, index) => {
+            if (val.indexOf('c') === 0 || val === 'sh0.08' || val === 's640x640')
+                return false;
+            else
+                return true;
+        }).join('/');
+        download(url);
+    });
 
 }
 
-var downloadVideo = () => {
-	var videoSelector = document.querySelectorAll(instagramVideoClassName);
-	// Iterate the images and call download function
-	Array.prototype.forEach.call(videoSelector, (el, i) => {
-		var url = el.getAttribute('src');
-		download(url);
-	});
-	
+var downloadVideos = () => {
+    var videoSelector = document.querySelectorAll(instagramVideoClassName);
+    // Iterate the videos and call download function
+    Array.prototype.forEach.call(videoSelector, (el, i) => {
+        var url = el.getAttribute('src');
+        download(url);
+    });
 }
